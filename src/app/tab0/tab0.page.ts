aa3ae4a6399596b6f1ce96df68ff9d9c0da8f6f1
Sebastian Arguello <sebaescu@espol.edu.ec>
2025-01-06 07:43:09 -0500

    se agrego los videos para las señas y se editó la pestaña de acerca de

--- src/app/tab0/tab0.page.ts
+++ src/app/tab0/tab0.page.ts
@@ -38,25 +38,28 @@ export class Tab0Page {
       description: 'Expresión usada para despedirse de alguien.',
       tutorial: 'La mano se desplaza desde un costado de la frente hacia delante. La cabeza se reclina ligeramente y la boca se abre.',
       imageUrl: 'assets/imagenes/adios.png',
+      videoUrl: 'assets/videos/adios.mp4'
     },
     {
       name: 'Seña de Buenos Días',
       description: 'Expresión de saludo usada para desear a alguien un día agradable.',
       tutorial: 'La mano desde la frente se desplaza hacia abajo y después hacia arriba mientras se abre simultáneamente.',
       imageUrl: 'assets/imagenes/buenosDias.png',
+      videoUrl: 'assets/videos/buenosDias.mp4',
     },
     {
       name: 'Seña de Gracias',
       description: 'Expresión usada para demostrar agradecimiento.',
       tutorial: 'La mano se separa del mentón hacia la palma contaría. El rostro sonríe.',
       imageUrl: 'assets/imagenes/gracias.png',
+      videoUrl: 'assets/videos/gracias.mp4',
     },
   ];
 
   viewDetails(sign: any) {
     this.navCtrl.navigateForward('/sign-details', {
       queryParams: {
-        sign: JSON.stringify(sign) // Pasamos el objeto completo
+        sign: JSON.stringify(sign)
       }
     });
   }
